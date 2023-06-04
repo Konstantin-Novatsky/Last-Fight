df590ba0516602b7cae0c1fb8a22a8fd6f33a015
Konstantin <superninja302@yandex.ru>
2023-06-04 16:35:31 +1000

    Version 0.2

---
+++ Last Fight/Assets/Scripts/PickUpWeapon.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpWeapon : MonoBehaviour
+{
+    public GameObject camera;
+    public float distance;
+    GameObject currentWeapon;
+    bool canPickUp;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) PickUp();
+        if (Input.GetKeyDown(KeyCode.Q)) Drop();
+    }
+
+    void PickUp()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, distance))
+        {
+            if (hit.transform.tag == "Weapon")
+            {
+                if (canPickUp) Drop();
+
+                currentWeapon = hit.transform.gameObject;
+                currentWeapon.GetComponent<Rigidbody>().isKinematic = true;
+                currentWeapon.transform.parent = transform;
+                currentWeapon.transform.localPosition = Vector3.zero;
+                currentWeapon.transform.localEulerAngles = new Vector3(10f, 0f, 0f);
+                canPickUp = true;
+            }
+        }
+    }
+
+    void Drop()
+    {
+        currentWeapon.transform.parent = null;
+        currentWeapon.GetComponent<Rigidbody>().isKinematic = false;
+        canPickUp = false;
+        currentWeapon = null;
+    }
+}
